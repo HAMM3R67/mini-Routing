ae9e50f19a184ab1a91f731e0cc0fb7475c11de9
HAMM3R67 <tcschauer@gmail.com>
2015-08-11 19:08:59 -0600

    added the home, settings and products routes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 var app = angular.module('miniRouting', ['ngRoute']);
+
+app.config(function($routeProvider){
+	.when('/', {
+		templateUrl: 'js/home/homeTmple.html',
+		controller: 'js/home/homeCtrl.js'
+	})
+	.when('/settings', {
+		templateUrl: 'js/settings/settingsTmpl.html',
+		controller: 'js/settings/settingsCtrl.js'
+	})
+	.when('/products/:id', {
+		templateUrl: 'js/products/productsTmpl.html',
+		controller: 'js/products/productsCtrl.js'
+	})
+})
